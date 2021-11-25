b1790a62d8d439da98b0c22eeb038742699a9122
Luis Tormes <luisangeltormes@gmail.coom>
2021-11-25 10:28:27 -0400

    corregi algunos errores en el componente cardNews, replanzando el img por el componete Image de nextjs

--- components/news/CardNews.js
+++ components/news/CardNews.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from "react";
 import styles from "../../styles/news.module.css";
 
@@ -5,7 +6,7 @@ const CardNews = ({ article }) => {
     return (
         <div className={styles.card}>
             <a href={article.url} target="_blank" className={styles.card_title}>{article.title} </a>
-            <a href={article.url} target="_blank">
+            <a href={article.url} target="_blank" rel="noreferrer">
                 <img src={article.urlToImage} alt='image' />
             </a>
             <p>{article.description}</p>
